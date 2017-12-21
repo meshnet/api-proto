a210ea3ea2d451e3aece92d5964329ed304dd2ce
Unknown <jetse@verschurendegroot.nl>
2017-12-21 19:06:46 +0100

    Began working at improved server tester

--- client-test.js
+++ client-test.js
@@ -3,14 +3,36 @@ const WebSocket = require('ws');
 const ws = new WebSocket('ws://localhost:8080');
 
 ws.on('open', function open() {
-  ws.send('{"action":"send","data":{"address":"PixelyIon","message":"Hey"}}');
-  ws.send('{"action":"send","data":{"address":"","message":"Hey"}}');
-  ws.send('{"action":"send","data":{"address":"PixelyIon","message":""}}');
-  ws.send('{"action":"error","data":{"address":"PixelyIon","message":"Hey"}}');
-  ws.send('{"data":{"address":"PixelyIon","message":"Hey"}}');
-  ws.send('{"action":"battery","data":{"random":"useless data but still accessable"}}');
+  this.received = "";
+
+  ws.on('message', (function incoming(data) {
+    this.received = data;
+  }).bind(this));
+
+  test(ws, {"action":"send","data":{"address":"PixelyIon","message":"Hey"}}, true);
 });
 
-ws.on('message', function incoming(data) {
-  console.log(data);
-});
+/*
+ws.send('{"action":"send","data":{"address":"PixelyIon","message":"Hey"}}');
+ws.send('{"action":"send","data":{"address":"","message":"Hey"}}');
+ws.send('{"action":"send","data":{"address":"PixelyIon","message":""}}');
+ws.send('{"action":"error","data":{"address":"PixelyIon","message":"Hey"}}');
+ws.send('{"data":{"address":"PixelyIon","message":"Hey"}}');
+ws.send('{"action":"battery","data":{"random":"useless data but still accessable"}}');
+*/
+
+function test(ws, message, expected, error) {
+  var sucess = true;
+  console.log("Sending " + JSON.stringify(message));
+  ws.send(JSON.stringify(message));
+  while(this.received === "");
+  console.log(this.received);
+  if(JSON.parse(this.received)["success"] !== expected)
+    success = false;
+  if(expected === false && JSON.parse(this.received)["error"] !== error)
+    success = false;
+  if(success)
+    console.log("Test passed!");
+  else
+    console.error("Test Failed!");
+}
